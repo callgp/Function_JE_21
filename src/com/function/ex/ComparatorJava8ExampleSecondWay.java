39fd0e30ec4c37a7a763d308b31e38c103e4bbb1
callgp <callgp@gmail.com>
2024-08-11 22:01:43 -0400

    first commit

---
+++ src/com/function/ex/ComparatorJava8ExampleSecondWay.java
@@ -0,0 +1,29 @@
+package com.function.ex;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+public class ComparatorJava8ExampleSecondWay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Bank> list=new ArrayList<>();
+		list.add(new Bank(234,"mike"));
+		list.add(new Bank(222,"john"));
+		list.add(new Bank(111,"json"));
+		list.add(new Bank(566,"marta"));
+	//	Function<Bank, Integer> f=bank->bank.getId();
+		
+	//	Comparator<Bank> comparator=Comparator.comparing(f);
+	//	Comparator<Bank> comparator=Comparator.comparing(bank->bank.getId());
+		//Comparator<Bank> comparator=Comparator.comparing(Bank::getId());
+		Comparator<Bank> comparator=Comparator.comparing(Bank::getCustomerName());
+		Collections.sort(list, comparator);
+		list.forEach(System.out::println);
+
+	}
+
+}
